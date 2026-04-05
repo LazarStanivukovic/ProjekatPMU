937c78114bcb33f9e0ffd5be403730e3a8bd53d6
LazarStanivukovic <stanivukoviclazar3@gmail.com>
2026-04-06 00:06:03 +0200

    fixed sync screen

--- app/src/main/java/com/example/projekat/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/projekat/ui/screens/settings/SettingsScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -92,7 +93,8 @@ fun SettingsScreen(
                     IconButton(onClick = onBack) {
                         Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Nazad")
                     }
-                }
+                },
+                windowInsets = WindowInsets(0, 0, 0, 0)
             )
 
             Column(
